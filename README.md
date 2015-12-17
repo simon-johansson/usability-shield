44249f2cf752767f6dd37ac914f2d0731ee47243
Simon Johansson <sijo@MacBook-Pro-3.local>
2015-12-17 23:55:27 +0100

    Proof of concept WIP

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # usability-shield
+
+[![Usability status][usability-image]][usability-url]
+
+## Test
+
+<!-- [usability-image]: https://img.shields.io/badge/usability_tested-yes!-FF41A2.svg -->
+[usability-image]: http://l:3000/shield.svg
+[usability-url]: http://l:3000/simon-johansson/Quizify
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
+var GitHubApi = require("github");
+var marked = require('marked');
+
 var router = express.Router();
 
-/* GET home page. */
+var github = new GitHubApi({
+  version: "3.0.0",
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
+});
+
+router.get('/shield.svg', function(req, res, next) {
+  res.redirect('https://img.shields.io/badge/usability_measures-taken-FF41A2.svg');
+});
+
+router.get('/repo/:user/:repo', function(req, res, next) {
+  var user = req.params.user;
+  var repo = req.params.repo;
+
+  github.repos.getContent({
+    user: user,
+    repo: repo,
+    path: 'README.md'
+  }, function (err, data) {
+    if (err) console.log(err);
+
+    if (data) {
+      var buf = new Buffer(data.content, 'base64');
+
+      res.render('repo', {
+        user: user,
+        repo: repo,
+        content: marked(buf.toString('ascii'))
+      });
+    } else {
+
+      res.redirect('/');
+    }
+  });
 });
 
 module.exports = router;
