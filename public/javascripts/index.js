e0e46f7b5fdcb58e1385289790b01159e8ea5f54
Simon Johansson <mail@simon-johansson.com>
2015-12-21 23:51:54 +0100

    Make possible to download markdown doc

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,9 @@
 
-var simplemde = new SimpleMDE();
+var $downloadBtn = $('button.download');
+
+$downloadBtn.on('click', function() {
+  event.preventDefault();
+
+  var blob = new Blob([simplemde.value()], {type: "text/markdown;charset=utf-8"});
+  saveAs(blob, "usability.md");
+});
