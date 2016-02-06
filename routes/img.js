2805bb1858cc19716f1c9f758977922ba9094edd
Simon Johansson <mail@simon-johansson.com>
2016-02-06 14:23:46 +0100

    Fix to /img route

--- routes/img.js
+++ routes/img.js
@@ -2,13 +2,13 @@
 import {Router} from 'express';
 const router = Router();
 
-router.get('/', function(req, res, next) {
-  res.redirect('https://img.shields.io/badge/usability_measures-taken-FF41A2.svg');
-});
+// router.get('/', function(req, res, next) {
+//   res.redirect('https://img.shields.io/badge/usability_measures-taken-FF41A2.svg');
+// });
 
 router.get('/:custom', (req, res, next) => {
   const {custom} = req.params;
-  res.redirect(`https://img.shields.io/badge/${custom}`);
+  res.redirect(`https://img.shields.io/badge/${custom}-FF41A2.svg`);
 });
 
 export default router;
